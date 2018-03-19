c671273b298ce594dbe56dfa22610b6ec1625254
Zach Mitchell <zmitchell@fastmail.com>
2018-03-19 11:15:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Async Serial
+
+This is example code showing you how to do asynchronous serial communication such as ASCII-over-serial, as is common in scientific settings. There is an accompanying article here:
+
+* TBD
+
+## Usage
+
+This code relies on the experimental [`pyserial-asyncio`][pyserial-asyncio] library, which at this time only supports Unix-based systems (Linux, macOS, etc). As explained in the article, you'll need the tool [`socat`][socat] to create virtual serial ports so that you don't need a real device to try out the code.
+
+### Install the dependencies
+
+Clone this repository:
+
+```bash
+git clone https://github.com/zmitchell/async-serial.git
+```
+
+The dependencies are managed with [`pipenv`][pipenv], the new officially sanctioned packaging tool for Python. Install the dependencies via:
+
+```bash
+pipenv install
+```
+
+This will create a virtual environment and install the dependencies listed in the [`Pipfile`](Pipfile) into it.
+
+### Run the codee
+
+You run the code in the virtual environment created by `pipenv` via
+
+```bash
+pipenv run python3 async_serial_protocol.py
+```
+
+or
+
+```bash
+pipenv run python3 async_serial_streams.py
+```
+
+## License
+
+Licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally
+submitted for inclusion in the work by you, as defined in the Apache-2.0
+license, shall be dual licensed as above, without any additional terms or
+conditions.
+
+[pyserial-asyncio]: http://pyserial-asyncio.readthedocs.io/en/latest/index.html
+[socat]: http://www.dest-unreach.org/socat/
+[pipenv]: https://github.com/pypa/pipenv
---
+++ async_serial_protocol.py
@@ -0,0 +1,65 @@
+import asyncio
+import serial
+import serial_asyncio
+
+
+class Reader(asyncio.Protocol):
+    def connection_made(self, transport):
+        """Store the serial transport and prepare to receive data.
+        """
+        self.transport = transport
+        self.buf = bytes()
+        self.msgs_recvd = 0
+        print('Reader connection created')
+
+    def data_received(self, data):
+        """Store characters until a newline is received.
+        """
+        self.buf += data
+        if b'\n' in self.buf:
+            lines = self.buf.split(b'\n')
+            self.buf = lines[-1]  # whatever was left over
+            for line in lines[:-1]:
+                print(f'Reader received: {line.decode()}')
+                self.msgs_recvd += 1
+        if self.msgs_recvd == 4:
+            self.transport.close()
+
+    def connection_lost(self, exc):
+        print('Reader closed')
+
+
+class Writer(asyncio.Protocol):
+    def connection_made(self, transport):
+        """Store the serial transport and schedule the task to send data.
+        """
+        self.transport = transport
+        self.buf = bytes()
+        print('Writer connection created')
+        asyncio.ensure_future(self.send())
+        print('Writer.send() scheduled')
+
+    def connection_lost(self, exc):
+        print('Writer closed')
+
+    async def send(self):
+        """Send four newline-terminated messages, one byte at a time.
+        """
+        message = b'foo\nbar\nbaz\nqux\n'
+        for b in message:
+            await asyncio.sleep(0.5)
+            self.transport.serial.write(bytes([b]))
+            print(f'Writer sent: {bytes([b])}')
+        self.transport.close()
+
+
+loop = asyncio.get_event_loop()
+reader = serial_asyncio.create_serial_connection(loop, Reader, 'reader', baudrate=115200)
+writer = serial_asyncio.create_serial_connection(loop, Writer, 'writer', baudrate=115200)
+asyncio.ensure_future(reader)
+print('Reader scheduled')
+asyncio.ensure_future(writer)
+print('Writer scheduled')
+loop.call_later(10, loop.stop)
+loop.run_forever()
+print('Done')
